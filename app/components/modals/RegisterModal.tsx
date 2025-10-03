6be9f03fd6291df7804b87738a3c308184bdd581
Mike Rembo <rembomike@gmail.com>
2025-10-03 09:59:56 +0300

    main components

---
+++ app/components/modals/RegisterModal.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import useRegisterModal from '@/app/hooks/useRegisterModal';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import Modal from './Modal';
+
+const RegisterModal = () => {
+  const registerModal = useRegisterModal();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    axios
+      .post('/api/register', data)
+      .then(() => {
+        registerModal.onClose();
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  const bodyContent = <div className="flex">Body</div>;
+
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={registerModal.isOpen}
+      title="Register"
+      actionLabel="Continue"
+      onClose={registerModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+    />
+  );
+};
+
+export default RegisterModal;
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Nunito } from 'next/font/google';
 import './globals.css';
 import Navbar from './components/navbar/Navbar';
 import ClientOnly from './components/ClientOnly';
+import Modal from './components/modals/Modal';
+import RegisterModal from './components/modals/RegisterModal';
 
 const font = Nunito({
   subsets: ['latin'],
@@ -22,6 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         {/* <ClientOnly> */}
+        <RegisterModal />
         <Navbar />
         {/* </ClientOnly> */}
         {children}
