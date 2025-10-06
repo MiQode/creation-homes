602b93167b71088c9a784f3215e2e7b78a627502
Mike Rembo <rembomike@gmail.com>
2025-10-06 13:29:17 +0300

    register success

--- app/api/auth/[...nextauth].ts
+++ app/api/auth/[...nextauth].ts
@@ -1,18 +1,18 @@
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import NextAuth, { AuthOptions } from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
-import Github from 'next-auth/providers/github';
-import Google from 'next-auth/providers/google';
 import bcrypt from 'bcrypt';
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
 
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
-    Github({
+    GithubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
-    Google({
+    GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
--- app/libs/prismadb.ts
+++ app/libs/prismadb.ts
@@ -1,5 +1,6 @@
 // import { PrismaClient } from '@prisma/client';
-import { PrismaClient } from '@prisma/client/extension';
+// import { PrismaClient } from '@prisma/client/extension';
+import { PrismaClient } from '../generated/prisma/client';
 
 declare global {
   var prisma: PrismaClient | undefined;
