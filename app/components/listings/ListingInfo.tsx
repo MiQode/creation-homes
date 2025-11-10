5b53b795c1794e5473c826d3281bfae99382775b
Mike Rembo <rembomike@gmail.com>
2025-11-10 13:15:02 +0300

    minor

--- app/components/listings/ListingInfo.tsx
+++ app/components/listings/ListingInfo.tsx
@@ -44,10 +44,10 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
   return (
     <div className="col-span-4 flex flex-col gap-8">
       <div className="flex flex-col gap-2">
-        <div className="text-xl font-semibold flex flex-row items-center gap-2">
+        {/* <div className="text-xl font-semibold flex flex-row items-center gap-2">
           <div className="">Hosted by {user?.name}</div>
           <Avatar src={user?.image} />
-        </div>
+        </div> */}
         <div className="flex flex-row items-center gap-4 font-light text-neutral-500">
           <div>{guestCount} guests</div>
           <div>{roomCount} rooms</div>
@@ -64,8 +64,8 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
       )}
       <hr />
       <div className="text-lg font-light text-neutral-500">{description}</div>
-      <hr />
-      <Map center={coordinates} />
+      {/* <hr />
+      <Map center={coordinates} /> */}
     </div>
   );
 };
