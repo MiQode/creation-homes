1e9bb70b6681d499bd97fcd06f4d3fdeae90e359
Mike Rembo <rembomike@gmail.com>
2026-04-20 22:00:39 +0300

    changed db

--- app/libs/prismadb.ts
+++ app/libs/prismadb.ts
@@ -1,11 +1,18 @@
-// import { PrismaClient } from '../generated/prisma/client';
 import { PrismaClient } from '@prisma/client';
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const client = globalThis.prisma || new PrismaClient();
-if (process.env.NODE_ENV === 'production') globalThis.prisma = client;
+const client =
+  globalThis.prisma ||
+  new PrismaClient({
+    datasourceUrl: process.env.DATABASE_URL,
+  });
+
+// Fix the environment logic (important)
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prisma = client;
+}
 
 export default client;
